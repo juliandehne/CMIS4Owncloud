b3dedf353508af49129a5cca6239bfec29c257bc
uzuzjmd <julian.dehne@gmail.com>
2014-11-17 15:58:43 +0100

    removing old filerepository approach

--- opencmisowncloud/src/main/java/org/up/ple/dcim4owncloud/util/MemFileSystem.java
+++
@@ -1,9 +0,0 @@
-package org.up.ple.dcim4owncloud.util;
-
-import java.io.FileNotFoundException;
-
-public interface MemFileSystem {
-	void writeFile(String name, byte[] contents);
-
-	byte[] readFile(String name) throws FileNotFoundException;
-}
